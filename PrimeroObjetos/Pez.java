a38d30bff3d625528e1f0586be52fb029b075f8f
Maximiliano Eschoyez <31444199+meschoyez@users.noreply.github.com>
2018-02-19 12:22:52 -0300

    Add files via upload

---
+++ PrimeroObjetos/Pez.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Veterinaria;
+
+/**
+ *
+ * @author meschoyez
+ */
+public class Pez {
+    private String raza;
+    private int edad;
+    private String color;
+    private String nombre;
+    private Tamano tamano;
+    private boolean aguaDulce;
+    private int tempAgua;
+    
+}
